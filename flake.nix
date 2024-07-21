7f0b83788f56fdd9ef7d0447676ff68694557de9
Alexandra Østermark <alex.cramt@gmail.com>
2024-07-21 14:39:19 +0200

    better nix setup

--- flake.nix
+++ flake.nix
@@ -1,42 +1,89 @@
 {
+  description = "homelab discord bot";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-          nativeBuildInputs = with pkgs; [
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+
+        sqlFilter = path: _type: null != builtins.match ".*sql$" path;
+        sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
+
+        src = lib.cleanSourceWith {
+          src = ./.;
+          filter = sqlOrCargo;
+          name = "source";
+        };
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+
+          buildInputs = [
+            pkgs.openssl
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+            pkgs.darwin.apple_sdk.frameworks.Security
+          ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        my-crate = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+
+          nativeBuildInputs = (commonArgs.nativeBuildInputs or [ ]) ++ [
+            pkgs.sqlx-cli
+          ];
+
+          preBuild = ''
+            export DATABASE_URL=sqlite:./db.sqlite3
+            sqlx database create
+            sqlx migrate run
+          '';
+        });
+      in
+      {
+        checks = {
+          inherit my-crate;
+        };
+
+        packages = {
+          default = my-crate;
+          inherit my-crate;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+
+          packages = with pkgs; [
             sqlx-cli
             bacon
-            rustToolchain
-            pkg-config
           ];
-          buildInputs = with pkgs; [ openssl ];
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            inherit buildInputs nativeBuildInputs;
 
-            shellHook = ''
-              touch secrets.env
-              export $(grep -v '^#' secrets.env | xargs -d '\n')
-            '';
-          };
-        }
-      );
+          shellHook = ''
+            touch secrets.env
+            export $(grep -v '^#' secrets.env | xargs -d '\n')
+          '';
+        };
+      });
 }
