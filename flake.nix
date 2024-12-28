03cc454fb8cc798a528bd0384cc90832ee2d2ced
Alexandra Østermark <alex.cramt@gmail.com>
2024-12-28 20:07:24 +0100

    big update time

--- flake.nix
+++ flake.nix
@@ -6,85 +6,129 @@
 
     crane = {
       url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+      inherit (pkgs) lib;
 
-        inherit (pkgs) lib;
+      craneLib = (crane.mkLib pkgs).overrideToolchain (
+        p:
+          p.rust-bin.stable.latest.default.override {
+            targets = [
+              "thumbv6m-none-eabi"
+              "x86_64-unknown-linux-gnu"
+            ];
+          }
+      );
 
-        craneLib = crane.mkLib pkgs;
+      src = craneLib.cleanCargoSource ./.;
 
-        sqlFilter = path: _type: null != builtins.match ".*sql$" path;
-        sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
 
-        src = lib.cleanSourceWith {
-          src = ./.;
-          filter = sqlOrCargo;
-          name = "source";
-        };
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
 
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
-
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
-
-          buildInputs = [
+        buildInputs =
+          [
             pkgs.openssl
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [
             pkgs.libiconv
             pkgs.darwin.apple_sdk.frameworks.Security
           ];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      individualCrateArgs =
+        commonArgs
+        // {
+          inherit cargoArtifacts;
+          inherit (craneLib.crateNameFromCargoToml {inherit src;}) version;
+          # NB: we disable tests since we'll run them all via cargo-nextest
+          doCheck = false;
         };
 
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-        homelab_discord_bot = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-
-          nativeBuildInputs = (commonArgs.nativeBuildInputs or [ ]) ++ [
-            pkgs.sqlx-cli
+      fileSetForCrate = crate:
+        lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            (craneLib.fileset.commonCargoSources ./common)
+            (craneLib.fileset.commonCargoSources crate)
+            ./host/migrations
           ];
+        };
+
+      host = craneLib.buildPackage (individualCrateArgs
+        // {
+          pname = "host";
+          cargoExtraArgs = "-p host";
+          src = fileSetForCrate ./host;
+
+          nativeBuildInputs =
+            (commonArgs.nativeBuildInputs or [])
+            ++ [
+              pkgs.sqlx-cli
+            ];
 
           preBuild = ''
             export DATABASE_URL=sqlite:./db.sqlite3
             sqlx database create
-            sqlx migrate run
+            sqlx migrate run --source host/migrations
           '';
         });
-      in
-      {
-        checks = {
-          inherit homelab_discord_bot;
-        };
+    in {
+      checks = {
+        inherit host;
+      };
 
-        packages = {
-          default = homelab_discord_bot;
-          inherit homelab_discord_bot;
-        };
+      packages = {
+        inherit host;
+      };
 
-        devShells.default = craneLib.devShell {
-          checks = self.checks.${system};
+      devShells.default = craneLib.devShell {
+        checks = self.checks.${system};
 
-          packages = with pkgs; [
-            sqlx-cli
-            bacon
-          ];
+        packages = with pkgs; [
+          sqlx-cli
+          bacon
+          probe-rs-tools
+          usbutils
+          elf2uf2-rs
+          picotool
+          screen
+        ];
 
-          shellHook = ''
-            touch secrets.env
-            export SYSTEMCTL_PATH=${pkgs.systemd}/bin/systemctl
-            export $(grep -v '^#' secrets.env | xargs -d '\n')
-          '';
-        };
-      });
+        shellHook = ''
+          touch secrets.env
+          export SYSTEMCTL_PATH=${pkgs.systemd}/bin/systemctl
+          export $(grep -v '^#' secrets.env | xargs -d '\n')
+        '';
+      };
+    });
 }
